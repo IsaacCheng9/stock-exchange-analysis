6d992394f117de452283403716dc7d5c960810ec
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-11-30 16:21:06 +0000

    Add initial files

---
+++ data_analysis.py
@@ -0,0 +1,14 @@
+"""
+Data Set: https://www.kaggle.com/datasets/mattiuzc/stock-exchange-data?select=indexProcessed.csv
+
+Ideas:
+    - Find common measures such as volatility and momentum.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
