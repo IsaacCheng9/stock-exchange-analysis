158b2abe43ae98e7f430a91d85e08602c60fc455
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-12-01 16:08:56 +0000

    Add stock data split

--- data_analysis.py
+++ data_analysis.py
@@ -8,20 +8,31 @@ Ideas:
     - Compare the results from LASSO and ridge regression - LASSO should be
       better according to papers?
 """
+from typing import List
+
 import pandas as pd
 
 
 def main():
-    print(load_stock_data("data/index_processed.csv"))
+    stock_data = load_stock_data("data/index_processed.csv")
+    training_data, test_data = split_data_set(stock_data)
+    print(training_data)
+    print(test_data)
 
 
 def load_stock_data(file_name: str) -> pd.DataFrame:
     stock_data = pd.read_csv(file_name)
-    return stock_data.head()
+    return stock_data
 
 
-def get_stock_prices():
-    pass
+def split_data_set(
+    stock_data: pd.DataFrame, training_proportion: float = 0.8
+) -> List[pd.DataFrame]:
+    split_index = int(len(stock_data) * training_proportion)
+    # TODO: Improve the stock data split, as it's currently not random.
+    training_data = stock_data[:split_index]
+    test_data = stock_data[split_index:]
+    return [training_data, test_data]
 
 
 if __name__ == "__main__":
