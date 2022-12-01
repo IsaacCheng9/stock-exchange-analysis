b592406845fc74bc40230a42efcf91a88a5126a7
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-12-01 15:18:14 +0000

    Load stock data

--- data_analysis.py
+++ data_analysis.py
@@ -3,10 +3,24 @@ Data Set: https://www.kaggle.com/datasets/mattiuzc/stock-exchange-data?select=in
 
 Ideas:
     - Find common measures such as volatility and momentum.
+    - Use LASSO regression for stock market forecasting.
+    - Use ridge regression for stock market forecasting.
+    - Compare the results from LASSO and ridge regression - LASSO should be
+      better according to papers?
 """
+import pandas as pd
 
 
 def main():
+    print(load_stock_data("data/index_processed.csv"))
+
+
+def load_stock_data(file_name: str) -> pd.DataFrame:
+    stock_data = pd.read_csv(file_name)
+    return stock_data.head()
+
+
+def get_stock_prices():
     pass
 
 
