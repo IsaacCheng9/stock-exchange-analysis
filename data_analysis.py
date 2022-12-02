67e6c6b06a72fc1ccd552d6f0773e370b000bf69
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-12-02 22:47:50 +0000

    Generate time series graph for each index

--- data_analysis.py
+++ data_analysis.py
@@ -8,7 +8,9 @@ Ideas:
     - Compare the results from LASSO and ridge regression - LASSO should be
       better according to papers?
 """
+import matplotlib.pyplot as plt
 import pandas
+import seaborn
 from sklearn.model_selection import train_test_split
 
 
@@ -16,21 +18,33 @@ def main():
     stock_data = load_stock_data("data/index_processed.csv")
     train_test_per_index = split_train_test_per_index(stock_data)
     print(train_test_per_index.keys())
+    for index in train_test_per_index.keys():
+        generate_time_series_graph_for_index(stock_data, index)
+    plt.show()
 
 
 def load_stock_data(file_name: str) -> pandas.DataFrame:
+    """
+    Load the stock data from the given CSV file.
+
+    Args:
+        file_name: The name of the CSV file to load the data from.
+
+    Returns:
+        The stock data as a pandas DataFrame.
+    """
     stock_data = pandas.read_csv(file_name)
     return stock_data
 
 
 def split_train_test_per_index(
-    data_set: pandas.DataFrame, training_proportion: float = 0.8
+    data: pandas.DataFrame, training_proportion: float = 0.8
 ) -> dict:
     """
-    Splits the data set into training and test data sets for each stock index.
+    Split the data set into training and test data sets for each stock index.
 
     Args:
-        data_set: The data set to split.
+        data: The data set to split.
         training_proportion: The proportion of the data set to be used for the
                              training data set.
 
@@ -39,8 +53,7 @@ def split_train_test_per_index(
     """
     # Split the data set by 'Index' column.
     data_frames_per_index = {
-        key: data_set.loc[value]
-        for key, value in data_set.groupby("Index").groups.items()
+        key: data.loc[value] for key, value in data.groupby("Index").groups.items()
     }
     # Generate a training and test data set for each index.
     train_test_per_index = {}
@@ -53,16 +66,31 @@ def split_train_test_per_index(
     return train_test_per_index
 
 
+def generate_time_series_graph_for_index(data: pandas.DataFrame, index: str):
+    """
+    Generate a time series graph for the given index.
+
+    Args:
+        data: The data set to get the index from.
+        index: The index to generate the graph for.
+    """
+    index_data = data.loc[data["Index"] == index]
+    plt.subplots(figsize=(10, 5))
+    axis = seaborn.lineplot(x="Date", y="Adj Close", data=index_data)
+    axis.set_title(f"Time Series Graph for {index}")
+    axis.set(xlabel="Date (YYYY-MM-DD)", ylabel="Adjusted Close Price")
+
+
 def build_lasso_regression_model(training_data: pandas.DataFrame):
     """
-    Builds a LASSO regression model to predict stock market prices.
+    Build a LASSO regression model to predict stock market prices.
     """
     pass
 
 
 def build_ridge_regression_model(training_data: pandas.DataFrame):
     """
-    Builds a ridge regression model to predict stock market prices.
+    Build a ridge regression model to predict stock market prices.
     """
     pass
 
